2a5bd62dbbbe26ec8f809bfcfc8a21551bc0749f
cals <calsmao@88vip.site>
2026-09-07 12:16:38 +0800

    fix(request_log): show input tokens instead of total in token column

--- lib/page/request_log/request_log_page.dart
+++ lib/page/request_log/request_log_page.dart
@@ -161,7 +161,7 @@ class _RequestLogPageState extends State<RequestLogPage> {
             var log = logs[index.row];
             var time = DateTime.fromMillisecondsSinceEpoch(log.timestamp);
             var statusCode = log.statusCode;
-            var tokenText = '${log.totalInputTokens} / ${log.outputTokens}';
+            var tokenText = '${log.inputTokens ?? 0} / ${log.outputTokens}';
             Widget child;
             switch (index.column) {
               case 0:
